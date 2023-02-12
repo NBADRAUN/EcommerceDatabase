9ed1de02c854e6d610e381feb56f5dc43502c675
NBADRAUN <Nbadraun@gmail.com>
2023-02-12 00:42:18 -0700

    inital model builds

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,42 @@
-// import important parts of sequelize library
+//// global includes ////
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// model //
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true 
+    }, 
+    product_name: {
+      type: DataTypes.STRING, 
+      allowNull: false 
+    }, 
+    product_price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false, 
+      defaultValue: 0
+    }, 
+    product_stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      defaultValue: 0 
+    }, 
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      reference: {
+        model: 'category',
+        key: 'category_id'
+      }
+    },
+
   },
   {
     sequelize,
--- seeds/product-tag-seeds.js
+++ seeds/product-tag-seeds.js
@@ -3,7 +3,10 @@ const { ProductTag } = require('../models');
 ///Product Tag Seed // 
 
 const productTagData = [
-  {product_id: 1, tag_id: 6,},
+  {
+    product_id: 1, 
+    tag_id: 6,
+  },
   {product_id: 1,tag_id: 7,},
   {product_id: 1,tag_id: 8,},
   {product_id: 2,tag_id: 6,},
